dbd13724a4d5fbe8d83ed355859969f1db756edd
SOMACAL <crdacostacaio@gmail.com>
2024-03-23 15:46:41 -0300

    Programa

---
+++ Programa.java
@@ -0,0 +1,31 @@
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Ex1.executar();
+        Ex2.executar2();
+        Ex3.executar3();
+        Ex4.executar4();
+        Ex5.executar5();
+        Ex6.executar6();
+        Ex7.executar7();
+        Ex8.executar8();
+        Ex9.executar9();
+        Ex10.executar10();
+        Ex11.executar11();
+        Ex12.executar12();
+        Ex13.executar13();
+        Ex14.executar14();
+        Ex15.executar15();
+        Ex16.executar16();
+        Ex17.executar17();
+        Ex18.executar18();
+        Ex19.executar19();
+        Ex20.executar20();
+
+
+
+
+    }
+
+}
